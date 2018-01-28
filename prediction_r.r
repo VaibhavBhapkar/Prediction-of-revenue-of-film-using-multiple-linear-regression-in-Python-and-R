8c35ea9d69918991503ed5dc8f5b61df21761d01
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2018-01-28 16:26:38 +0530

    Add files via upload

---
+++ prediction_r.r
@@ -0,0 +1,25 @@
+# Multiple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('C:\\Users\\Vaibhav\\Desktop\\Machine Learning Dataset Implementation\\tmdb-5000-movie-dataset\\tmdb_5000_movies.csv')
+dataset=dataset[c('budget','popularity','runtime','vote_average','vote_count','revenue')]
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$revenue, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Multiple Linear Regression to the Training set
+regressor = lm(formula = revenue ~ .,
+               data = training_set)
+summary(regressor)
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
